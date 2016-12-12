182d9c3c233b592798e4b1d8474b7bc1d853851a
andrew <computedscientist@gmail.com>
2016-12-12 16:16:00 -0500

    added sql query to test database connection

--- app.py
+++ app.py
@@ -6,13 +6,27 @@
     --Date--
     December 12, 2016
 
-    UI for the recur prototype
+    Recur application prototype
 """
 from tkinter import *
 import pymysql
 from modules.system.SystemData import SystemData
 from modules.database.dbconfig import dbconnect
 
+# Connect to the database
 db = dbconnect()
 
-print("database: {0}".format(db))
+# Test that the database connection works
+# def dbtest():
+#     try:
+#         with db.cursor() as cur:
+#             query = """SELECT * FROM User"""
+#             cur.execute(query)
+#             query_results = cur.fetchall()
+#             print("database connection test: {0}".format(query_results))
+#     except Exception as e:
+#         print("DEBUG - Exception occurred: {0}".format(e))
+#     finally:
+#         db.close()
+#
+# dbtest()
